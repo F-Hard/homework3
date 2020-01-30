dcc0ee19905040552615fac071b85b93e2d137ee
符式鸿 <835791080@qq.com>
2020-01-30 22:18:31 +0800

    添加项目文件。

---
+++ homework3/Test.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Test.aspx.cs" Inherits="Test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            请输入一个正整数： 
+            <asp:TextBox ID="TextBox1" runat="server" Width="103px"></asp:TextBox>
+&nbsp;&nbsp;
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="确定" />
+            <br />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ homework3/Test.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Test : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        String show = "";
+        Boolean flag = true; // "..."开关
+        int result = 0;
+        int i = 0;
+        int goal_temp = 1;
+        int goal_number = Convert.ToInt32(TextBox1.Text);
+
+        for(i=1; i<= goal_number; i++)
+        {
+            for(int j=2;j<=i;j++)
+            {
+                goal_temp = goal_temp * j;
+            }
+            result = result + goal_temp;
+            goal_temp = 1;
+        }
+
+        if (goal_number == 0)
+            Label1.Text = "0！= 1";
+        else
+        {
+            for (i = 1; i < goal_number; i++)
+            {
+                if (i == 3 && goal_number < 4)
+                {
+                    show += i.ToString() + "!";
+                }
+                else if (i <= goal_number - 1 && i <= 3)
+                    show += i.ToString() + "! + "; 
+                else if (i > 3 && flag)
+                {
+                    show += " ...... +";
+                    flag = false;
+                }       
+            }
+            if(i != 4)
+                show += i.ToString() + "! = " + result.ToString();
+            else
+            {
+                show += " ...... +"+i.ToString()+" = " +result.ToString();
+            }
+         Label1.Text = show;  
+        }
+        result = 0;
+    }
+}
